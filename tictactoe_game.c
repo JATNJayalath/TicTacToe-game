97e2d1c55d833d46c5b05927b045d71246c3f8a2
JATNJayalath <it25102766@my.sliit.lk>
2025-10-01 21:50:14 +0530

    Assignment

--- tictactoe_game.c
+++ tictactoe_game.c
@@ -177,7 +177,7 @@ void computer_move(char** board, int n, int *row, int *column) {
 
 void Two_player_game(int n) {
 	char** board = initialize_board(n);
-    	FILE* logFile = fopen("game.txt", "w");
+    	FILE* logFile = fopen("game.txt", "a");
 
     	char symbol[2] = {'X', 'O'};
     	int turn = 0; 
@@ -218,9 +218,11 @@ void Two_player_game(int n) {
     	free(board);
 }
 
+//2. User VS Computer Mode
+
 void User_VS_Computer_game(int n) {
 	char** board = initialize_board(n);
-	FILE* logFile = fopen("game.txt", "w");
+	FILE* logFile = fopen("game.txt", "a");
 
 	char symbol[2] = {'X', 'O'};
 	int turn = 0;
@@ -273,7 +275,67 @@ void User_VS_Computer_game(int n) {
 
 }
 
+//3. Multiplayer Mode
+
 void Multiplayer_game(int n) {
+	char** board = initialize_board(n);
+	FILE* logFile = fopen("game.txt", "a");
+
+	char symbol[3] = {'X', 'O', 'Z'};
+	int human_or_computer[3] = {0, 0, 0};      //0-human, 1-computer
+	int turn = 0;
+	int row, column;
+	int game_over = 0;
+
+	//Player 1 must be human and Ask for player 2 and 3 whether they are human or computer moves
+	human_or_computer[0] = 0;
+
+	printf("Is player 2 a human or computer (0-human, 1-computer): ");
+    	scanf("%d", &human_or_computer[1]);
+    	printf("Is player 3 a human or computer (0-human, 1-computer): ");
+    	scanf("%d", &human_or_computer[2]);
+
+	while(!game_over) {
+		display_board(board, n);
+
+		//human move
+		if(human_or_computer[turn] == 0) {
+			printf("Player %c, enter your move (row column): ", symbol[turn]);
+			scanf("%d %d", &row, &column);
+
+			if(!is_valid_move(board, n, row, column)) {
+				printf("Invalid move! Try again.\n");
+				continue;
+			}
+		}
+		//Computer move
+		else {
+			computer_move(board, n, &row, &column);
+			printf("Computer %c playes at (row column): %d %d\n", symbol[turn], row, column);
+		}
+		board[row][column] = symbol[turn];
+		log_file(logFile, board, n);
+
+		if(check_win(board, n, symbol[turn])) {
+			display_board(board, n);
+			printf("Player %c wins\n", symbol[turn]);
+			game_over = 1;
+		}
+		else if(check_draw(board, n)){
+			display_board(board, n);
+			printf("It's a draw\n");
+			game_over = 1;
+		}
+		else {
+			turn = (turn+1)%3;
+		}
+	}
+	fclose(logFile);
+	for(int i=0; i<n; i++) {
+		free(board[i]);
+	}
+	free(board);
+		
 }
 
 
