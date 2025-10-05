5572ad6ca71ee0e90ec04b4e189dd101e9df744f
JATNJayalath <it25102766@my.sliit.lk>
2025-10-05 20:21:19 +0530

    Assignment

--- tictactoe_game.c
+++ tictactoe_game.c
@@ -1,19 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-char** initialize_board(int n);
-void display_board(char** board, int n);
-void get_user_input(int *row, int *column, int n);
-int is_valid_move(char** board, int n, int row, int column);
-int check_win(char** board, int n, char symbol);
-int check_draw(char** board, int n);
-void log_file(FILE* logFile, char** board, int n);
-void computer_move(char** board, int n, int *row, int *column);
-
-//Game Modes
-void Two_player_game(int n);
-void User_VS_Computer_game(int n);
-void Multiplayer_game(int n);
+#include "tictactoe.h"
 
 int main() {
 	int n;
